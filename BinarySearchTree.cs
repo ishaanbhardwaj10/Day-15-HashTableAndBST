f52c89c7aa9a959d6f176e3d04ef5c098a8efcd1
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-22 14:08:02 +0530

    BST UC-1: added Insert and Display methods

---
+++ BinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBST
+{
+    internal class BinarySearchTree<T> where T : IComparable<T>
+    {
+        MyBinaryNode<T> rootNode;
+        BinarySearchTree<T> leftChildTree { get; set; }
+        BinarySearchTree<T> rightChildTree { get; set; }
+        internal BinarySearchTree(T nodeData)
+        {
+            rootNode = new MyBinaryNode<T>(nodeData);
+            leftChildTree = null;
+            rightChildTree = null;
+        }
+        
+
+        public void Insert(T item)
+        {
+
+            T currentNodeValue = this.rootNode.NodeData;
+            if(currentNodeValue.CompareTo(item) > 0)
+            {
+                if(leftChildTree == null)
+                {
+                    leftChildTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    leftChildTree.Insert(item);
+                }
+            }
+            else
+            {
+                if(rightChildTree == null)
+                {
+                    rightChildTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    rightChildTree.Insert(item);
+                }
+            }
+
+        }
+
+
+        public void Display()
+        {
+
+            if(leftChildTree != null)
+            {
+                leftChildTree.Display();
+            }
+
+            Console.WriteLine(this.rootNode.NodeData.ToString());
+
+            if(rightChildTree != null)
+            {
+                rightChildTree.Display();
+            }
+
+        }
+
+    }
+}
