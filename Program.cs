120a647ecbbf5fdb6cc6a454cc891e516b3701b7
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-22 15:21:09 +0530

    BST UC-2: added UC-2 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
 
             Console.WriteLine("Welcome to HashTable and Binary Search Tree Problems");
             Console.WriteLine("Use Case #1: Create a Binary Search Tree");
+            Console.WriteLine("Use Case #2: Create the Binary Tree shown in figure");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +19,25 @@
                     treeOne.Insert(30);
                     treeOne.Insert(70);
                     treeOne.Display();
+                    break;
+                case 2:
+                    BinarySearchTree<int> treeTwo = new BinarySearchTree<int>(56);
+                    treeTwo.Insert(30);
+                    treeTwo.Insert(70);
+                    treeTwo.Insert(22);
+                    treeTwo.Insert(40);
+                    treeTwo.Insert(60);
+                    treeTwo.Insert(95);
+                    treeTwo.Insert(11);
+                    treeTwo.Insert(65);
+                    treeTwo.Insert(3);
+                    treeTwo.Insert(16);
+                    treeTwo.Insert(63);
+                    treeTwo.Insert(67);
+                    treeTwo.Display();
+                    Console.WriteLine("-------------------------------");
+                    treeTwo.GetSize();
+                    Console.WriteLine("-------------------------------");
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
